0588fad31d3b84e2bf22952c1ca55a7f471310a1
Hanna <Hanna_Zaneuskaya@epam.com>
2020-03-16 23:08:40 +0300

    feat: implement morse-decoder

--- src/index.js
+++ src/index.js
@@ -1,45 +1,76 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
+const SPACE = "**********";
+
 function decode(expr) {
-    // write your solution here
+    let result = "";
+
+    for (let index = 0; index < expr.length; index += 10) {
+
+        let letter = expr.slice(index, index + 10);
+        if (letter === SPACE) {
+            result = result + " ";
+            continue;
+        }
+
+        let morseElement = "";
+        for (let letterIndex = 0; letterIndex < letter.length; letterIndex += 2) {
+            let element = letter.slice(letterIndex, letterIndex + 2);
+            if (element === "10") {
+                morseElement = morseElement + ".";
+                continue;
+            }
+
+            if (element === "11") {
+                morseElement = morseElement + "-";
+                continue;
+            }
+        }
+
+        let morseLetter = MORSE_TABLE[morseElement];
+        result = result + morseLetter;
+    }
+
+    return result;
 }
+
 
 module.exports = {
     decode
